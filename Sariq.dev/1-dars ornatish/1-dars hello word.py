19e999945791ac88bcba09d85c876f353be56250
Doniyor Saydiivaliyev <81846742+mrdonrich@users.noreply.github.com>
2022-11-04 14:21:40 +0500

    Sariq dev darslari
    Papkaga yangi darslik yuklandi

---
+++ Sariq.dev/1-dars ornatish/1-dars hello word.py
@@ -0,0 +1,10 @@
+# print("Hello word")
+# Muallif Saydivaliyev Doniyor
+
+import tkinter as tk
+rasm = tk.Tk()
+rasm.title(" Doniyor ")
+rasm.geometry("1000x500")
+
+rasm.config(bg = "DimGray")
+rasm.mainloop()
