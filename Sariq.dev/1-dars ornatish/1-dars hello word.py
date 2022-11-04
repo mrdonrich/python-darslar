45bc880f127cb465018cd8743106fda5ac56b14c
Doniyor Saydiivaliyev <81846742+mrdonrich@users.noreply.github.com>
2022-11-04 14:27:11 +0500

    Update 1-dars hello word.py

--- Sariq.dev/1-dars ornatish/1-dars hello word.py
+++ Sariq.dev/1-dars ornatish/1-dars hello word.py
@@ -1,10 +1,4 @@
 # print("Hello word")
 # Muallif Saydivaliyev Doniyor
 
-import tkinter as tk
-rasm = tk.Tk()
-rasm.title(" Doniyor ")
-rasm.geometry("1000x500")
-
-rasm.config(bg = "DimGray")
-rasm.mainloop()
+print("Salom Dunyo")
